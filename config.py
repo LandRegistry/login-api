e6eae374f2714f62496c6e25699c8c59462e7883
Camille Baldock <camille@camillebaldock.co.uk>
2015-03-25 17:36:46 +0000

    Disable logging when running unit tests

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ logging_config_file_path = os.environ['LOGGING_CONFIG_FILE_PATH']
 
 CONFIG_DICT = {
     'DEBUG': False,
+    'LOGGING': True,
     'LOGGING_CONFIG_FILE_PATH': logging_config_file_path,
     'SQLALCHEMY_DATABASE_URI': os.environ['SQLALCHEMY_DATABASE_URI'],
     'PASSWORD_SALT': os.environ['PASSWORD_SALT'],
@@ -15,5 +16,6 @@ settings = os.environ.get('SETTINGS')
 if settings == 'dev':
     CONFIG_DICT['DEBUG'] = True
 elif settings == 'test':
+    CONFIG_DICT['LOGGING'] = False
     CONFIG_DICT['DEBUG'] = True
     CONFIG_DICT['TESTING'] = True
--- service/__init__.py
+++ service/__init__.py
@@ -1,11 +1,13 @@
 import logging
 from logging import config
 import json
+from config import CONFIG_DICT
 
 def setup_logging(logging_config_file_path):
-    try:
-        with open(logging_config_file_path, 'rt') as file:
-            config = json.load(file)
-        logging.config.dictConfig(config)
-    except IOError as e:
-        raise (Exception('Failed to load logging configuration', e))
+    if CONFIG_DICT['LOGGING']:
+        try:
+            with open(logging_config_file_path, 'rt') as file:
+                config = json.load(file)
+            logging.config.dictConfig(config)
+        except IOError as e:
+            raise (Exception('Failed to load logging configuration', e))
