fa4b6e63b047bad0b8559488381421d712f12763
Leszek Gonczar <gonczar@gmail.com>
2015-05-08 13:56:34 +0000

    Log events from application's lifecycle

---
+++ service/logging_config.py
@@ -0,0 +1,20 @@
+import logging
+from logging import config
+import json
+from config import CONFIG_DICT
+
+done_setup = False
+
+
+def setup_logging():
+    global done_setup
+
+    if not done_setup and CONFIG_DICT['LOGGING']:
+        try:
+            logging_config_file_path = CONFIG_DICT['LOGGING_CONFIG_FILE_PATH']
+            with open(logging_config_file_path, 'rt') as file:
+                config = json.load(file)
+            logging.config.dictConfig(config)
+            done_setup = True
+        except IOError as e:
+            raise(Exception('Failed to load logging configuration', e))
