2d5ce11be6c698ca2c3dbb4b5eb473a1a969eb4e
lgonczar <gonczar@gmail.com>
2015-05-22 10:34:58 +0000

    Store application fault details in log file

--- config.py
+++ config.py
@@ -1,14 +1,19 @@
 import os
 
 logging_config_file_path = os.environ['LOGGING_CONFIG_FILE_PATH']
+fault_log_file_path = os.environ['FAULT_LOG_FILE_PATH']
+sqlalchemy_database_uri = os.environ['SQLALCHEMY_DATABASE_URI']
+password_salt = os.environ['PASSWORD_SALT']
+port = os.environ['PORT']
 
 CONFIG_DICT = {
     'DEBUG': False,
     'LOGGING': True,
     'LOGGING_CONFIG_FILE_PATH': logging_config_file_path,
-    'SQLALCHEMY_DATABASE_URI': os.environ['SQLALCHEMY_DATABASE_URI'],
-    'PASSWORD_SALT': os.environ['PASSWORD_SALT'],
-    'PORT': os.environ['PORT'],
+    'FAULT_LOG_FILE_PATH': fault_log_file_path,
+    'SQLALCHEMY_DATABASE_URI': sqlalchemy_database_uri,
+    'PASSWORD_SALT': password_salt,
+    'PORT': port,
 }
 
 settings = os.environ.get('SETTINGS')
@@ -19,3 +24,4 @@ elif settings == 'test':
     CONFIG_DICT['LOGGING'] = False
     CONFIG_DICT['DEBUG'] = True
     CONFIG_DICT['TESTING'] = True
+    CONFIG_DICT['FAULT_LOG_FILE_PATH'] = '/dev/null'
--- service/__init__.py
+++ service/__init__.py
@@ -5,8 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 from service import logging_config
 from service import security
 
-# This causes the traceback to be written to stderr in case of faults
-faulthandler.enable()
+# This causes the traceback to be written to the fault log file in case of serious faults
+fault_log_file = open(CONFIG_DICT['FAULT_LOG_FILE_PATH'], 'a')
+faulthandler.enable(file=fault_log_file)
 
 app = Flask(__name__)
 app.config.update(CONFIG_DICT)
