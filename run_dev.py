94561ae615172550c8d1e48a220ca5174e345aea
Nick Gravgaard <nick@beautifulcode.net>
2015-08-03 16:59:20 +0100

    Add type annotations

--- run_dev.py
+++ run_dev.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import atexit
 import logging
 from service.server import run_app
@@ -8,7 +7,7 @@ LOGGER = logging.getLogger(__name__)
 
 @atexit.register
 def handle_shutdown(*args, **kwargs):
-    LOGGER.info("Stopped the server")
+    LOGGER.info('Stopped the server')
 
-LOGGER.info("Starting the server")
+LOGGER.info('Starting the server')
 run_app()
